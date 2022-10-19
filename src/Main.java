64f88dd0aa52bfca29a51f4d2e6883990e67db9d
Gabriel <gabrigabriel1010@gmail.com>
2022-10-19 10:40:43 +0100

    Exercise 1 finished

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        Scanner teclado = new Scanner(System.in);
+        ProcessBuilder pb = new ProcessBuilder("notepad.exe");
+        Process process = null;
+
+
+        System.out.println("¿Cuantos procesos quieres?");
+        int numeroTeclado = teclado.nextInt();
+
+        ArrayList<Process> cargadeProcesos = new ArrayList<>();
+
+        for (int i = 0; i < numeroTeclado; i++) {
+            process = pb.start();
+            cargadeProcesos.add(process);
+        }
+
+        for (int i = 0; i < cargadeProcesos.size(); i++) {
+            Process misProcesos = cargadeProcesos.get(i);
+            while (misProcesos.isAlive()) {}
+        }
+
+        try {
+            int exitCode = process.waitFor();
+            System.out.println("procesos terminados " + exitCode);
+        } catch (InterruptedException e) {
+            System.out.println("Ta petao");
+            System.err.println("El proceso no se ha podido ejecutar");
+            System.err.println("Esto hizo Cascaso");
+        }
     }
 }
